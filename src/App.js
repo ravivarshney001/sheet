1bc8fdfc6ffb5068f521f6067cb9e55d4d0cc948
shubhamkumar33 <shubham.suneshwar.kumar@testbook.com>
2024-09-25 14:51:57 +0530

    sheet flow updating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import './App.css';
-import {useRef} from 'react';
-
+import { useRef } from 'react';
+import { useEffect } from 'react';
 import { Workbook } from "@fortune-sheet/react";
-import "@fortune-sheet/react/dist/index.css"
+import "@fortune-sheet/react/dist/index.css";
+
 function App() {
-  const ref = useRef(null) 
+  const ref = useRef(null);
+
+  const PostMessageListener = () => {
+    useEffect(() => {
+      const handleMessage = (event) => {
+        console.log('Message received:', event.data);
+
+        const allowedOrigin = "testbookSheetData";
+        if (event.origin !== allowedOrigin) {
+          return;
+        }
+
+        if (event.data && event.data.tid) {
+          console.log(event.data);
+          if (ref.current) {
+            const sheetData = ref.current.getSheet();
+            window.parent.currentSheetData = sheetData;
+            window.sheetData = sheetData;
+            console.log(sheetData);
+          }
+        }
+
+        console.log('Validated message:', event.data);
+      };
+      window.addEventListener('message', handleMessage);
+
+      return () => {
+        window.removeEventListener('message', handleMessage);
+      };
+    }, []);
+  };
+
+  PostMessageListener();
+
   const handleOnClick = () => {
-    console.log(ref.current.getSheet())
-  }
+    if (ref.current) {
+      const sheetData = ref.current.getSheet();
+      window.currentSheetData = sheetData;
+      console.log(sheetData, 'sheet data on click');
+    }
+  };
+
   return (
     <>
-    <Workbook data={[{ name: "Sheet1" }]} ref={ref} />
-    <button onClick={handleOnClick}>Get Data</button>
+      <Workbook data={[{ name: "Sheet1" }]} ref={ref} />
+      <button onClick={handleOnClick}>Get Data</button>
     </>
   );
 }
